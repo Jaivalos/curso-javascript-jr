f781e71fd32f7181960acf2885151318b864644f
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-14 21:18:11 -0600

    Compuertas basicas

--- 01 introduccion/codigo.js
+++ 01 introduccion/codigo.js
@@ -1,23 +1,8 @@
+// Compuertas basicas
 
-// Operadores logicos y de comparacion
+let valor = true;
+let valor2 = false;
 
-let numero = 23;
-let numero2 = 13;
-
-//Preguntar si son iguales
-document.write(numero == numero2);
-
-//Preguntar si son distintos
-document.write(numero != numero2);
-
-//Preguntar si son estrictamente iguales
-document.write(numero === numero2);
-
-//Preguntar si son estrictamente distintos
-document.write(numero !== numero2);
-
-//Mayor, menor, mayor igual o menor igual
-document.write(numero > numero2);
-document.write(numero < numero2);
-document.write(numero >= numero2);
-document.write(numero <= numero2);
+let resultado = valor && valor2;
+let resultado = valor || valor2;
+let resultado = !valor;
