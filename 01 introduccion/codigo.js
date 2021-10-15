676cc2ca34d34bcfadd75b113166b8ef701961fd
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-14 20:28:35 -0600

    Ejemplo simple de prompt

--- 01 introduccion/codigo.js
+++ 01 introduccion/codigo.js
@@ -1,6 +1,2 @@
-
-var, let, const //Formas de iniciar una variable
-
-/* var tiene un alcance global, mientras que let es mas para un uso local 
-    y const obviamente es para un valor constante*/
-
+let nombre = prompt("Decime tu nombre");
+alert("Hola " + nombre);
