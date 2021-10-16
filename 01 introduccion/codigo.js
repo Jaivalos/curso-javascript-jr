16912802a7e75395288bf035945ff5e029361d09
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-16 15:23:31 -0600

    Array aosciativo

--- 01 introduccion/codigo.js
+++ 01 introduccion/codigo.js
@@ -1,14 +1,20 @@
 
-// Array
+// Array asociativo
 
-// Es una variable con varios datos dentro
+let pc = {
+    nombre: "Hp ProBook",
+    procesador: "Intel core I7",
+    ram: "8Gb",
+    espacio: "128Gb"
+};
 
-let frutas = ["banana", "manzana", "pera", 5, 3];
+let frase = `El nombre de mi pc es: <b> ${pc["nombre"]} </b> <br>
+             El procesador de mi pc es: <b> ${pc["procesador"]} </b> <br>
+             La ram de mi pc es: <b> ${pc["ram"]} </b> <br>
+             El espacio de mi pc es: <b> ${pc["espacio"]} </b> <br>`;
 
-//Acceder a los elementos
-
-document.write(frutas[0]);
-document.write(frutas[1]);
-document.write(frutas[2]);
-document.write(frutas[3]);
-document.write(frutas[4]);
+document.write(frase);
+document.write(pc["nombre"]);
+document.write(pc["procesador"]);
+document.write(pc["ram"]);
+document.write(pc["espacio"]);
