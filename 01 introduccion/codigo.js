eab84ada0b9e8c304466acd0ab1b606841b310c2
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-16 15:31:50 -0600

    Ciclo whilw

--- 01 introduccion/codigo.js
+++ 01 introduccion/codigo.js
@@ -1,20 +1,10 @@
+// Ciclo while 
 
-// Array asociativo
+let numeroParaSumar = 0;
 
-let pc = {
-    nombre: "Hp ProBook",
-    procesador: "Intel core I7",
-    ram: "8Gb",
-    espacio: "128Gb"
+while(numeroParaSumar < 10){
+    numeroParaSumar++;
+    document.write(numeroParaSumar);
 };
 
-let frase = `El nombre de mi pc es: <b> ${pc["nombre"]} </b> <br>
-             El procesador de mi pc es: <b> ${pc["procesador"]} </b> <br>
-             La ram de mi pc es: <b> ${pc["ram"]} </b> <br>
-             El espacio de mi pc es: <b> ${pc["espacio"]} </b> <br>`;
 
-document.write(frase);
-document.write(pc["nombre"]);
-document.write(pc["procesador"]);
-document.write(pc["ram"]);
-document.write(pc["espacio"]);
