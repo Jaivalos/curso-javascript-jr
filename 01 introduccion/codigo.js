2c933f63774fbca0a36f62fc0e77dac0879bbf2a
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-17 17:12:25 -0600

    Funciones con parametros

--- 01 introduccion/codigo.js
+++ 01 introduccion/codigo.js
@@ -1,12 +1,29 @@
-//Return
+//Parametros
 
-//EL RETURN TERMINA LA FUNCION, ES COMO UN BREAK
-
-function saludar(){
-    alert("Hola");
-    return "Para eso sirve el return";
+function suma(num1, num2){
+    let res = num1 + num2;
+    document.write(res);
+    document.write(`<br>`);
 }
 
-let saludo = saludar();
+suma(12, 32);
+suma(22, 32);
 
-document.write(saludo);
+//Otro ejemplo con return
+
+function res(num1, num2){
+    let res = num1 + num2;
+    return (res + `<br>`);
+}
+
+let resultado = res(20, 25);
+document.write(resultado);
+
+//Ejemplo de saludar
+
+function saludar(nombre){
+    let saludo = `Hola ${nombre} como te va?`;
+    document.write(saludo);
+}
+
+saludar("Papagayo");
