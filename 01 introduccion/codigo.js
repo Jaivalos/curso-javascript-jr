51d244ba2403b2d097d383ba095692fbb7858ff6
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-23 14:40:07 -0600

    Historia de Cofla 4, pro 1
    Historia de Cofla 4, pro 1

--- 01 introduccion/codigo.js
+++ 01 introduccion/codigo.js
@@ -1,38 +1,67 @@
+//Ejemplo de calculadora por prompt
 
-const sumar = (num1, num2)=>{
-    return num1 + num2;
+class calculadora {
+    constructor(){
+    }
+    sumar = (num1, num2)=>{
+        return num1 + num2;
+    }
+    resta = (num1, num2)=>{
+        return num1 - num2;
+    }
+    mult = (num1, num2)=>{
+        return num1 * num2;
+    }
+    div = (num1, num2)=>{
+        return num1 / num2;
+    }
+    pot = (num1, exp)=>{
+        return num1 ** exp;
+    }
+    r2 = (num1)=>{
+        return Math.sqrt(num1);
+    }
+    r3 = (num1)=>{
+        return Math.cbrt(num1);
+    }
 }
-const resta = (num1, num2)=>{
-    return num1 - num2;
-}
-const mult = (num1, num2)=>{
-    return num1 * num2;
-}
-const div = (num1, num2)=>{
-    return num1 / num2;
-}
+
+const calc = new calculadora();
 
 alert(`Que operacion deseas realizar?`);
-let operacion = prompt(`1. Suma, 2. Resta, 3. Multiplicacion, 4. Division`);
+let operacion = prompt(`1. Suma, 2. Resta, 3. Multiplicacion, 4. Division, 5. Potencia a la x, 6. Raiz cuadrada, 7. Raiz cubica `);
 
 if(operacion == 1){
     let numero1 = prompt(`Ingresa el primer numero`);
     let numero2= prompt(`Ingresa el segudno numero`);
-    let res= sumar(numero1, numero2);  
+    let res= calc.sumar(numero1, numero2);  
     alert(`Tu resultado es: ${res}`);
 }else if(operacion == 2){
     let numero1 = prompt(`Ingresa el primer numero`);
     let numero2= prompt(`Ingresa el segudno numero`);
-    let res= resta(numero1, numero2);
+    let res= calc.resta(numero1, numero2);
     alert(`Tu resultado es: ${res}`);  
 }else if(operacion == 3){
     let numero1 = prompt(`Ingresa el primer numero`);
     let numero2= prompt(`Ingresa el segudno numero`);
-    let res= mult(numero1, numero2); 
+    let res= calc.mult(numero1, numero2); 
     alert(`Tu resultado es: ${res}`); 
 }else if(operacion == 4){
     let numero1 = prompt(`Ingresa el primer numero`);
     let numero2= prompt(`Ingresa el segudno numero`);
-    let res= div(numero1, numero2);  
+    let res= calc.div(numero1, numero2);  
+    alert(`Tu resultado es: ${res}`);
+}else if(operacion == 5){
+    let numero1 = prompt(`Ingresa la base`);
+    let numero2= prompt(`Ingresa el exponente`);
+    let res= calc.pot(numero1, numero2);  
+    alert(`Tu resultado es: ${res}`);
+}else if(operacion == 6){
+    let numero1 = prompt(`Ingresa la base`);
+    let res= calc.r2(numero1);  
+    alert(`Tu resultado es: ${res}`);
+}else if(operacion == 7){
+    let numero1 = prompt(`Ingresa la base`);
+    let res= calc.r3(numero1);  
     alert(`Tu resultado es: ${res}`);
 }else alert(`Numero no reconocido`);
