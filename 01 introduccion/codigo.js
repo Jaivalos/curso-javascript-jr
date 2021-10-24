b0f6d76069f1083dc36b1462dcc3e980ce030a45
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-24 12:28:40 -0600

    Funciones de conteo

--- 01 introduccion/codigo.js
+++ 01 introduccion/codigo.js
@@ -2,29 +2,13 @@
 
 La consola la podemos encontrar en un navegador con solo colocar inspeccionar
 
-Funciones de regitro*/
+Funciones de conteo*/
 
-//clear
-console.clear();
-
-//Assert
-console.assert(5>12);
-console.assert(5>2);
-
-//error
-console.error("Capo que hiciste?");
-
-//info
-console.info("Muy bien tu trabajo");
-
-//log
-console.log("Muy bien tu trabajo 2");
-
-//table
-console.table([4,5,6,3,3,55,,43,5324,4]);
-
-//warn
-console.warn("Debes cliquear para ejecutar");
-
-//dir
-console.dir([4,3,4,5,3,3]);
+//count
+console.count();
+console.count();
+console.count();
+console.count();
+console.count();
+console.countReset();
+console.count();
