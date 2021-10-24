690ab235b385562c2f1b08ae5275df1e614239ea
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-24 12:32:53 -0600

    Funciones de agrupacion

--- 01 introduccion/codigo.js
+++ 01 introduccion/codigo.js
@@ -2,13 +2,18 @@
 
 La consola la podemos encontrar en un navegador con solo colocar inspeccionar
 
-Funciones de conteo*/
+Funciones de agrupacion*/
 
-//count
-console.count();
-console.count();
-console.count();
-console.count();
-console.count();
-console.countReset();
-console.count();
+//group
+
+console.group("Master");
+console.log("Hola");
+console.table([4,5,4,34,3]);
+console.group("Segundo");
+console.log("Hola");
+console.table([4,5,4]);
+console.groupEnd();
+console.info("Maanna tienes que ir a la tienda");
+console.groupEnd();
+
+
