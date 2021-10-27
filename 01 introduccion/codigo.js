fa854f60183ae5ccee330b15e898b187da8359c0
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-26 22:13:16 -0600

    Metodos de seleccion de elementos

--- 01 introduccion/codigo.js
+++ 01 introduccion/codigo.js
@@ -1,44 +1,17 @@
-//Metodos de repeticion de array
 
-<<<<<<< HEAD
-let trabajo = "240 minutos para trabajar";
-let estudio = "100 minutos de estudio";
-let tp = '100 minutos de trabajos practicos';
-let housework = "30 minutos de tareas de la casa";
-let descanso = "10 minutos de descanso";
+/* El dom es todo lo relacionado con la construccion de nuestra pagina web, 
+es la forma en la que creamos y construimos nuestra pagina*/
 
-for(i = 1; i <= 14; i ++){
-    if(i == 1){
-        console.group("Semana 1");
-    }
-    console.log("Tareas");
-    console.group("Dia "+i);
-    console.log(trabajo);
-    console.log(estudio);
-    console.log(descanso);
-    console.log(tp);
-    console.log(housework);
-    console.groupEnd();
-    if(i == 7){
-        console.groupEnd();
-        console.group("Semana 2")
-    }
-}
-=======
-//filter()
+  //Metodos de seleccion de elementos
 
-let numeros = ["Uno","Dos","Tres","Cuatro","Cinco"];
+  //getElementById
+  let parrafo = document.getElementById("parrafo");
+  document.write(parrafo + "<br>");
 
-res = numeros.filter(numero => numero.length > 4);
-document.write(res + "<br>")
+  //getElementByTagName
+  parrafo = document.getElementsByTagName("p");
+  document.write(parrafo + "<br>");
 
-//Es un ciclo que obtendra cada uno de los elementos del array
-
-//forEach()
-
-let numeros2 = ["Uno","Dos","Tres","Cuatro","Cinco"];
-
-numeros2.filter(numero => document.write(numero + "<br>"));
-
-//Es un ciclo que obtendra cada uno de los elementos del array
->>>>>>> refs/remotes/origin/main
+  //querySelector
+  parrafo = document.querySelector("#parrafo");
+  document.write(parrafo + "<br>");
