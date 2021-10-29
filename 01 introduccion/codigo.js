c05aa9b399baec1ad71d42a6cf02a161bdb200dd
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-28 22:23:44 -0600

    Eliminar o agregar una clase dependiendo si exxiste o no

--- 01 introduccion/codigo.js
+++ 01 introduccion/codigo.js
@@ -2,5 +2,4 @@
 //Clases Classlist y 
 
 const input = document.querySelector(".clase1");
-document.write(input.classList.contains("rojo"));
-document.write(input.classList.contains("Jjd"));
+input.classList.toggle("mana")
