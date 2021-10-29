d57ab2a4c6ac83a8cfd8f1c930d2d33e8516161d
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-28 22:21:02 -0600

    Verificar si el objeto contiene una clase en especial

--- 01 introduccion/codigo.js
+++ 01 introduccion/codigo.js
@@ -2,5 +2,5 @@
 //Clases Classlist y 
 
 const input = document.querySelector(".clase1");
-document.write(input.classList.item(1));
-document.write(input.classList.item(2));
+document.write(input.classList.contains("rojo"));
+document.write(input.classList.contains("Jjd"));
