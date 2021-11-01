bfdee7e44d8f43f72a4b32313f7eabe3d1dc7114
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-31 20:27:57 -0600

    Remplaza un hijo por otro

--- 01 introduccion/codigo.js
+++ 01 introduccion/codigo.js
@@ -1,28 +1,13 @@
 
+//METODOS DE CHILDS 
+
+//Remplazar un hijo
+
 const cont = document.querySelector(".contenedor");
 
-//Obtener el primer hijo 
-let primHijo = cont.firstChild;
+const pnuevo = document.createElement('P');
+pnuevo.innerHTML = "Parrafo";
 
-//Obtener el ultimo hijo 
-let ultHijo = cont.lastChild;
+pviejo = document.querySelector(".pantiguo");
 
-//Obtener el primer elemento hijo 
- primHijo = cont.firstElementChild;
-
-//Obtener el ultimo elemento hijo 
- ultHijo = cont.lastElementChild;
-
- // LISTA DE LOS HIJOS 
-let nodeList = cont.childNodes;
-
-//Recorrer los nodos
-nodeList.forEach(hijo => console.log(hijo));
-
-// lISTA DE LOS OBJETOS HTML 
-let element = cont.children;
-
-//Recorrer los nodos del children
-for (const hijo of element) {
-    console.log(hijo);
-}
+cont.replaceChild(pnuevo,pviejo);
