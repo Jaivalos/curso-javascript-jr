a9cf4753b8be6147e1972b15285ced8032225512
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-31 22:26:52 -0600

    Historia final de cofla

--- 01 introduccion/codigo.js
+++ 01 introduccion/codigo.js
@@ -1,15 +1,33 @@
 
-//METODOS DE CHILDS 
+const cont = document.querySelector(".flex-container");
+let contador = 0;
 
-//Eliminar un hijo
+function crearLlave(nombre, modelo, precio){
+    contador ++;
+    img =  `<img src="llave.png">`;
+    nombre = `<h2>${nombre}</h2>`
+    modelo = `<h3 id="${modelo}">${modelo}</h3>`
+    precio = `<p> ${precio}</p>`
+    return [img, nombre, modelo, precio];
+}
 
-const cont = document.querySelector(".contenedor");
+const changeHidden = (number)=>{
+    document.querySelector(".key-data").value = number;
+}
 
-const pnuevo = document.createElement('P');
-pnuevo.innerHTML = "Parrafo";
+let documentFragment = document.createDocumentFragment();
 
-pviejo = document.querySelector(".pantiguo");
+for( i = 0; i < 20; i++){
+    let modeloRandom = Math.round(Math.random()*10000);
+    let precioRandom = Math.round(Math.random()*10 + 30);
+    let llave = crearLlave(`Llave: ${i + 1}`, `Modelo: ${modeloRandom}`, `Precio: ${precioRandom}`);
 
-cont.replaceChild(pnuevo,pviejo);
+    let div = document.createElement("DIV");
+    div.addEventListener("click", ()=>{changeHidden(modeloRandom)});
+    div.tabIndex = i;
+    div.classList.add(`item-${i + 1}`,`flex-item`);
+    div.innerHTML = llave[0] + llave[1] + llave[2] + llave[3];
+    documentFragment.appendChild(div);
+}
 
-cont.removeChild(pnuevo);
+cont.appendChild(documentFragment);
